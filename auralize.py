e0f5d5e810e7192ee5c89b118a471be5487c4133
Dan Suh <kaehops@gmail.com>
2019-08-30 21:33:25 +0900

    refined auralization module

--- auralize.py
+++ auralize.py
@@ -9,31 +9,44 @@ import librosa.display
 from scipy.io import wavfile
 
 
+def auralize(model_file='model.pt', iterate=200, is_cat=True):
+    model = CatsDogsModel3()
+    model.load_state_dict(torch.load(model_file, map_location='cpu'))
+    model.eval()
+
+    initial_image = torch.rand((1, 1, 16000))  # 1-sec of random samples
+    # allow parameter updates from back-propagation
+    initial_image.requires_grad = True
+    optimizer = optim.SGD([initial_image], lr=5, weight_decay=0.05)
+
+    target_name = 'cat' if is_cat else 'dog'
+    auralized = initial_image
+    # train the 'input' with respect to desired output target
+    # i.e. find the best input that yields the desired target
+    for i in range(iterate):
+        out = model(auralized)
+        if is_cat:
+            # we want the output to be close to 0 (cat label)
+            class_loss = out
+        else:
+            # we want the output to be close to 1 (dog label)
+            class_loss = (1 - out)
+
+        # back-prop to input
+        optimizer.zero_grad()
+        class_loss.backward()
+        optimizer.step()
+        print(f'loss: {class_loss}, is_cat: {is_cat}')
+
+        if i % 20 == 0:
+            plt.figure()
+            plt.plot(np.squeeze(initial_image.detach().numpy()))
+            plt.show()
+            wavfile.write(f'{target_name}_{i}.wav', 16000, np.squeeze(auralized.detach().numpy()))
+    return auralized
+
+
 if __name__ == '__main__':
-    # model = CatsDogsModel3()
-    # model.load_state_dict(torch.load('model.pt', map_location='cpu'))
-    # model.eval()
-
-    # initial_image = torch.rand((1, 1, 16000))
-    # initial_image.requires_grad = True
-    # optimizer = optim.SGD([initial_image], lr=5, weight_decay=0.05)
-
-    # for i in range(200):
-    #     out = model(initial_image)
-    #     class_loss = out  # cat
-
-    #     optimizer.zero_grad()
-    #     class_loss.backward()
-    #     optimizer.step()
-    #     print(class_loss)
-
-    #     if i % 20 == 0:
-    #         plt.figure()
-    #         plt.plot(np.squeeze(initial_image.detach().numpy()))
-    #         plt.show()
-    #         print(i)
-    #         wavfile.write(f'cat_{i}.wav', 16000, np.squeeze(initial_image.detach().numpy()))
-
     cat_samp, _ = librosa.load('cat_180.wav')
     plt.figure(figsize=(4, 8))
     D = librosa.amplitude_to_db(np.abs(librosa.stft(cat_samp)), ref=np.max)
